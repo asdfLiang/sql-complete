1c83a188831e482a4ac9050e62f12c9bb786ea57
asdfLiang <liangzj9624@163.com>
2023-09-30 15:03:50 +0800

    feat: 流程组件增加节点功能

--- deploy/src/main/java/com/liang/deploy/components/ProcessComponent.java
+++ deploy/src/main/java/com/liang/deploy/components/ProcessComponent.java
@@ -1,9 +1,12 @@
 package com.liang.deploy.components;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
 
@@ -20,34 +23,22 @@ public class ProcessComponent extends AnchorPane {
         super();
         this.PRE_WIDTH = preWidth;
 
+        // 创建基本流程
         VBox baseProcess = baseProcess();
-        this.getChildren().addAll(baseProcess);
+        this.getChildren().add(baseProcess);
 
+        // 整个组件始终居中
         this.widthProperty()
                 .addListener(
                         (observable, oldValue, newValue) -> {
                             if (baseProcess.getWidth() == 0) return;
-                            System.out.println(
-                                    "component listener, component width: "
-                                            + newValue.doubleValue()
-                                            + ", startButton width: "
-                                            + baseProcess.getWidth()
-                                            + ", layoutX: "
-                                            + (newValue.doubleValue() - baseProcess.getWidth())
-                                                    / 2);
                             baseProcess.setLayoutX(
                                     (newValue.doubleValue() - baseProcess.getWidth()) / 2);
                         });
     }
 
-    private VBox processNode(VBox parentNode) {
-        VBox vBox = new VBox();
-        vBox.setAlignment(Pos.CENTER);
-        TextArea textArea = sqlTextArea();
-        Button addButton = addButton(vBox);
-        Line line2 = new Line(0, 0, 0, 30);
-        vBox.getChildren().addAll(textArea, line2, addButton);
-        return vBox;
+    private Button startButton() {
+        return new Button("开始流程");
     }
 
     private VBox baseProcess() {
@@ -55,39 +46,68 @@ public class ProcessComponent extends AnchorPane {
         vBox.setAlignment(Pos.TOP_CENTER);
         vBox.setLayoutY(30);
 
+        // 结构定义
         Button startButton = startButton();
-        TextArea textArea = sqlTextArea();
-        Button addButton = addButton(null);
+        Node textArea = sqlTextArea();
+        Button addButton = addButton(vBox);
         Line line1 = new Line(0, 0, 0, 30);
         Line line2 = new Line(0, 0, 0, 30);
-        vBox.getChildren().addAll(startButton, line1, textArea, line2, addButton);
+        HBox next = new HBox();
 
+        // 组装
+        vBox.setUserData(next);
+        vBox.getChildren().addAll(startButton, line1, textArea, line2, addButton, next);
         vBox.setLayoutX((PRE_WIDTH - TEXT_AREA_WIDTH) / 2);
 
         return vBox;
     }
 
-    private Button startButton() {
-        return new Button("开始流程");
+    private VBox processNode(VBox parentNode) {
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.setLayoutY(parentNode.getLayoutY() + parentNode.getHeight() + 25);
+
+        // 结构定义
+        Node textArea = sqlTextArea();
+        textArea.setLayoutY(25);
+        Button addButton = addButton(vBox);
+        Line line2 = new Line(0, 0, 0, 30);
+        HBox next = new HBox();
+
+        // 组装
+        vBox.setUserData(next);
+        vBox.getChildren().addAll(textArea, line2, addButton, next);
+
+        return vBox;
     }
 
-    private TextArea sqlTextArea() {
+    private Node sqlTextArea() {
         TextArea textArea = new TextArea();
         textArea.setPrefWidth(TEXT_AREA_WIDTH);
         textArea.setPrefHeight(TEXT_AREA_HEIGHT);
+        textArea.setMaxWidth(TEXT_AREA_WIDTH);
 
         return textArea;
     }
 
-    private Button addButton(VBox parentNode) {
-        Button button = new Button("+");
+    /** 添加节点 */
+    private Button addButton(VBox preVBox) {
+        Button addButton = new Button("+");
 
-        button.setOnAction(event -> this.getChildren().add(processNode(parentNode)));
+        addButton.setOnAction(
+                event -> {
+                    VBox subNode = processNode(preVBox);
+                    subNode.setPadding(new Insets(30, 15, 0, 15));
+                    subNode.setLayoutY(preVBox.getLayoutY() + preVBox.getHeight() + 25);
 
-        return button;
-    }
+                    // 放到父节点下的集合中
+                    HBox sub = (HBox) preVBox.getUserData();
+                    sub.setLayoutY(30);
+                    sub.getChildren().add(subNode);
 
-    private Line linkLine(VBox start, VBox end) {
-        return new Line();
+                    // 边框着色...连线太难了，先整个看的懂的效果，后期优化
+                    sub.setStyle("-fx-border-color: black");
+                });
+        return addButton;
     }
 }
